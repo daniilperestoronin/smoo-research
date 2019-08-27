6bcb900eefc8f2f00a83020f0a6584e2bd8f1624
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2019-08-27 18:02:45 +0300

    Update alg3

--- alg3.py
+++ alg3.py
@@ -1,18 +1,17 @@
 import numpy as np
+import pandas as pd
 
-try:
-  import pandas as pd
-
-  returns = pd.read_csv('returns.txt', index_col=0)
-  assets = np.array(returns.columns)
-  dates = np.array(returns.index)
-  returns = returns.as_matrix()
-except ImportError:
-  returns = np.genfromtxt('returns.txt', delimiter=',')[1:, 1:]
-  with open('returns.csv') as f:
-    lines = f.readlines()
-  assets = np.array(lines[0][:-1].split(',')[1:])
-  dates = np.array([line[:10] for line in lines[1:]])
+returns = pd.read_csv('./data/returns.txt', index_col=0)
+assets = np.array(returns.columns)
+dates = np.array(returns.index)
+returns = returns.values
 T, n = returns.shape
-volumes = np.genfromtxt('volumes.txt', delimiter=',')[2:, 1:]
-prices = np.genfromtxt('prices.txt', delimiter=',')[2:, 1:]
+volumes = np.genfromtxt('./data/volumes.txt', delimiter=',')[2:, 1:]
+prices = np.genfromtxt('./data/prices.txt', delimiter=',')[2:, 1:]
+
+print(returns)
+print(assets)
+print(dates)
+
+print(volumes)
+print(prices)
