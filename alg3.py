fa3eb615307bc34dd88d4aeaf645b0ada7d56cca
daniilperestoronin <perestoronin.daniil@gmail.com>
2019-08-31 22:11:09 +0300

    Add graphs in alg3

--- alg3.py
+++ alg3.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
 
 returns = pd.read_csv('./data/returns.txt', index_col=0)
 assets = np.array(returns.columns)
@@ -9,9 +11,28 @@ T, n = returns.shape
 volumes = np.genfromtxt('./data/volumes.txt', delimiter=',')[2:, 1:]
 prices = np.genfromtxt('./data/prices.txt', delimiter=',')[2:, 1:]
 
-print(returns)
-print(assets)
-print(dates)
+################################################################################
+# Graphs
+################################################################################
 
-print(volumes)
-print(volumes)
+print(assets)
+
+plt.plot(returns)
+plt.title('Daily market returns for last 10 years')
+plt.xlabel('days')
+plt.legend(assets, loc=2, prop={'size': 2})
+plt.show()
+
+plt.plot(volumes)
+plt.title('Daily market volumes for last 10 years')
+plt.xlabel('days')
+plt.ylabel('dollars')
+plt.legend(assets, loc=2, prop={'size': 2})
+plt.show()
+
+plt.plot(prices)
+plt.title('Daily market close prices for last 10 years')
+plt.xlabel('days')
+plt.ylabel('dollars')
+plt.legend(assets, loc=2, prop={'size': 2})
+plt.show()
