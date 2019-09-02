e97674b689d022ea392c2e4c0f6225b6a441f2f6
daniilperestoronin <perestoronin.daniil@gmail.com>
2019-09-02 23:09:31 +0300

    Update alg3

--- alg3.py
+++ alg3.py
@@ -33,52 +33,54 @@ prob_n_ret = np.empty([T, n])
 for i in xrange(T):
     prob_n_ret[i] = np.sum(np.array(returns[:i, :]) <= 0, axis=0) / float(i + 1)
 
-print(prob_n_ret)
-
 ################################################################################
 # Graphs
 ################################################################################
 
-plt.plot(returns)
-plt.title('Daily market returns for last 10 years')
-plt.xlabel('days')
-plt.legend(assets, loc=2, prop={'size': 2})
-plt.show()
-
-plt.plot(volumes)
-plt.title('Daily market volumes for last 10 years')
-plt.xlabel('days')
-plt.ylabel('dollars')
-plt.legend(assets, loc=2, prop={'size': 2})
-plt.show()
-
-plt.plot(prices)
-plt.title('Daily market close prices for last 10 years')
-plt.xlabel('days')
-plt.ylabel('dollars')
-plt.legend(assets, loc=2, prop={'size': 2})
-plt.show()
-
-plt.plot(prob_n_ret)
-plt.title('The probability of a negative return for last 10 years')
-plt.xlabel('days')
-plt.legend(assets, loc=2, prop={'size': 2})
-plt.show()
+# plt.plot(returns)
+# plt.title('Daily market returns for last 10 years')
+# plt.xlabel('days')
+# plt.legend(assets, loc=2, prop={'size': 2})
+# plt.show()
+#
+# plt.plot(volumes)
+# plt.title('Daily market volumes for last 10 years')
+# plt.xlabel('days')
+# plt.ylabel('dollars')
+# plt.legend(assets, loc=2, prop={'size': 2})
+# plt.show()
+#
+# plt.plot(prices)
+# plt.title('Daily market close prices for last 10 years')
+# plt.xlabel('days')
+# plt.ylabel('dollars')
+# plt.legend(assets, loc=2, prop={'size': 2})
+# plt.show()
+#
+# plt.plot(prob_n_ret)
+# plt.title('The probability of a negative return for last 10 years')
+# plt.xlabel('days')
+# plt.legend(assets, loc=2, prop={'size': 2})
+# plt.show()
 
 ################################################################################
 # Linprog
 ################################################################################
 
+#####
+# Solve in mean
+#####
+
 # mean return
 m_returns = np.sum(returns, axis=0) / T
 
-# The probability of a negative return
-n_exp = np.sum(np.array(returns) <= 0, axis=0) / float(T)
+# mean probability of a negative return
+m_prob_n_ret = np.sum(prob_n_ret, axis=0) / float(T)
 
 alpha = 0.5
 gamma = 0.5
 
-vct = list(-alpha * m_returns + gamma * n_exp)
+vct = list(-alpha * m_returns + gamma * m_prob_n_ret)
 
 matrix = [[0 for y in xrange(n)] for x in xrange(n)]
 matrix[0] = m_returns
@@ -89,3 +91,27 @@ b[0] = 100000
 res = linprog(vct, A_eq=matrix, b_eq=b, method='interior-point')
 
 print(res)
+
+#####
+# Negative
+#####
+
+n_returns = np.amin(returns, axis=0)
+n_prob_n_ret = np.amin(prob_n_ret, axis=0)
+n_vct = list(-alpha * n_returns + gamma * n_prob_n_ret)
+
+n_res = linprog(n_vct, A_eq=matrix, b_eq=b, method='interior-point')
+
+print(n_res)
+
+#####
+# Positive
+#####
+
+p_returns = np.amax(returns, axis=0)
+p_prob_n_ret = np.amax(prob_n_ret, axis=0)
+p_vct = list(-alpha * p_returns + gamma * p_prob_n_ret)
+
+p_res = linprog(p_vct, A_eq=matrix, b_eq=b, method='interior-point')
+
+print(p_res)
